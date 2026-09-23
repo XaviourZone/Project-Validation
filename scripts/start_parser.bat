75d82d19ca7fcf3857a832b7b2822ee93678fdec
YASSIN XAVIOUR A <yasxanto@gmail.com>
2026-09-23 16:02:51 +0530

    Add independent service launcher

---
+++ scripts/start_parser.bat
@@ -0,0 +1,8 @@
+@echo off
+setlocal
+set "ROOT=%~dp0.."
+for %%I in ("%ROOT%") do set "ROOT=%%~fI"
+set "VALIDATION_HOME=%ROOT%"
+set "PYTHONPATH=%ROOT%;%PYTHONPATH%"
+cd /d "%ROOT%"
+py -3 -m parser.app.main --config parser/config/parser.yaml
